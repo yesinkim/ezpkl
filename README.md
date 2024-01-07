682f2dae5070a49932ee045fdddffde241ae5c52
Sage <bailando.ys@gmail.com>
2024-01-07 18:48:08 +0900

    📝 docs: add README and enhance package setup

---
+++ README.md
@@ -0,0 +1,39 @@
+# EZPKL: lovely pickling🥒 for context manager hater👊
+
+컨텍스트 매니저를 열고 닫는 것이 귀찮아서 만들었습니다.
+
+## 설치
+
+패키지 관리자 [pip](https://pip.pypa.io/en/stable/)를 사용하여 ezpkl를 설치합니다.
+
+```bash
+pip install ezpkl
+```
+
+## 사용법
+
+### 객체 저장
+
+```python
+from ezpkl import save_pkl
+
+a = [1, 2, 3, 4, 5]
+
+# 현재 폴더에 'a.pkl'이 저장됩니다.
+save_pkl(var=a)
+
+# 현재 폴더에 'a_list_temp.pkl'이 저장됩니다.
+save_pkl(var=a, file_name='a_list_temp')
+```
+
+### 객체 불러오기
+
+```python
+from ezpkl import load_pkl
+
+a = load_pkl(a)
+```
+
+## 라이선스
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import find_packages, setup
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="ezpkl",
     version="0.1.0",
     description="lovely pickling🥒 for context manager hater👊",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="bailando",
     author_email="bailando.ys@gmail.com",
     url="https://github.com/yesinkim/ezpkl.git",
-    packages=find_packages(exclude = []),
+    packages=find_packages(exclude=[]),
     keywords=["pickle", "file", "easy", "easypickle"],
     python_requires=">=3",
-    
 )
