3e051af9af266b092ebd3cb054cca4397e0a0c1f
Sage <bailando.ys@gmail.com>
2024-01-21 00:06:16 +0900

    feat: add ingredients

--- ezpkl/__init__.py
+++ ezpkl/__init__.py
@@ -1,3 +1,3 @@
 from ezpkl.pickling import load_pkl, save_pkl
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
---
+++ ezpkl/refrigerator.py
@@ -0,0 +1,7 @@
+ingredients = [
+    'apricot', 'broccoli', 'cherry', 'date', 'elderberry',
+    'fig', 'grape', 'honeydew', 'iceberg', 'jalapeno',
+    'kiwi', 'lime', 'mushroom', 'nectarine', 'olive',
+    'pumpkin', 'quince', 'raspberry', 'strawberry', 'tangerine',
+    'uglifruit', 'vanilla', 'watermelon', 'xigua', 'yam', 'zucchini'
+]
